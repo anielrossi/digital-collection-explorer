8493692b6d768ddb07dea3488b2070df9c063432
aniel <rssnla93@gmail.com>
2024-10-26 06:07:15 +0200

    working script

--- rekordbox_collection_explorer.py
+++ rekordbox_collection_explorer.py
@@ -6,10 +6,14 @@ import discogs_client
 import glob
 from mutagen.mp3 import MP3  
 from mutagen.easyid3 import EasyID3  
-from mutagen.id3 import ID3, TCOM, COMM
+from mutagen.id3 import ID3, TCON, COMM
 
-
+#Requirements:
+# all files mp3 for tagging standard
+# Rekordbox input file format
+"""
 root_dir = "/Volumes/SANDISK1/Contents"
+
 data = []
 
 # Iterate over one level of subdirectories and list their contents
@@ -30,7 +34,9 @@ styles = []
 genres = []
 labels = []
 
+print("start search")
 for idx,i in df.iterrows():
+    print(idx)
     try:
         release = d.search(i["Artist"] + " " + i["Album"], type='release')[0]
         labels.append(release.labels[0].name)
@@ -40,22 +46,22 @@ for idx,i in df.iterrows():
         labels.append("error")
         styles.append("error")
         genres.append("error")
+print("end search")
 
 df["Style"] = styles
 df["Genres"] = genres
 df["Labels"] = labels
 
 df.to_csv("df_rekordbox.csv")
+"""
+import pandas as pd
 
-# %%
-df["style"].value_counts()
+df = pd.read_csv('df_rekordbox.csv', index_col=0)
 
-# %%
-errors = df[df["labels"] == "error"]
+errors = df[df["Labels"] == "error"]
 df_final = df.drop(index=errors.index)
-merged_df = df_final.merge(df)
 
-merged_df.to_csv("df_clean_rekordbox.csv")
+df_final.to_csv("df_clean_rekordbox.csv")
 errors.to_csv('errors_rekordbox.csv')
 
 """
@@ -65,23 +71,27 @@ df_exploded[df_exploded["style"] == "Ambient"]
 df_exploded.to_csv("df_exploded_rekordbox.csv")
 """
 
-for idx, x in merged_df.iterrows():
-    print(idx, x["Path"])
+print("---------writing labels---------")
+for idx, x in df_final.iterrows():
     try:
         filez = glob.glob(os.path.join(x["Path"], '*.mp3'))
-        if (x["labels"]):
+        if (x["Labels"]):
             for i in filez:
                 mp3file = MP3(i, ID3=EasyID3)
-                mp3file["organization"] = x["labels"]
+                mp3file["organization"] = x["Labels"]
                 mp3file.save()
     except Exception as e:
         print("error:", e)
 
-for idx, x in merged_df.iterrows():
+print("---------writing genres---------")
+for idx, x in df_final.iterrows():
     try:
         filez = glob.glob(os.path.join(x["Path"], '*.mp3'))
         genres_str = ""
         if (x["Style"]):
+            #only if reading df
+            x["Style"] = x["Style"].strip('][').replace("'", "").split(', ')
+            ###
             for genre in x["Style"]:
                 genres_str += genre + "|"
             for i in filez:
@@ -91,20 +101,22 @@ for idx, x in merged_df.iterrows():
     except:
         print("error:", idx)
 
-for idx, x in merged_df.iterrows():
+print("---------writing comments---------")
+for idx, x in df_final.iterrows():
     try:
         filez = glob.glob(os.path.join(x["Path"], '*.mp3'))
         genres_str = ""
-        if x["style"]:
-            for genre in x["style"]:
+        if x["Style"]:
+            #only if reading df
+            x["Style"] = x["Style"].strip('][').replace("'", "").split(', ')
+            ###
+            for genre in x["Style"]:
                 genres_str += genre + "|"
-            print(genres_str)
             for i in filez:
                 mp3file = ID3(i)
-                #from old script
-                #mp3file["TCON"] = TCON(encoding=3, text=genre)
-                mp3file["COMM"] = COMM(encoding=3, text=genres_str[:-1])
-                #mp3file.add(COMM(encoding=3, text=genres_str[:-1]))
+                #from old file, don't know if needed
+                #mp3file["TCON"] = TCON(encoding=3, text=x["Style"][0])
+                mp3file.add(COMM(encoding=3, text=genres_str[:-1]))
                 mp3file.save()
     except:
         print("error:", idx)
